e48fe0429fd71eecbd4008369cb9ff27654a22cf
ImNotAki2014 <jiratthisut@gmail.com>
2026-04-13 12:08:24 +0700

    Create utils.py file in shared folder

---
+++ shared/utils.py
@@ -0,0 +1,27 @@
+def load_data(file_path):
+    """Load data from a given file path."""
+    pass
+
+def get_summary_data(data):
+    """Get summary data from the provided dataset."""
+    pass
+
+def send_alert_email(recipient, subject, body):
+    """Send an alert email to the specified recipient."""
+    pass
+
+def lock_mac():
+    """Lock the Mac."""
+    pass
+
+def check_lock():
+    """Check if the Mac is locked."""
+    pass
+
+def is_macos():
+    """Check if the operating system is macOS."""
+    return True
+
+def appkit_available():
+    """Check if the AppKit is available."""
+    return True
