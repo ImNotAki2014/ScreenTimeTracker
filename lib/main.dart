e7c71e6bd6963aee96d23df883d19b0289385c9e
ImNotAki2014 <jiratthisut@gmail.com>
2026-04-14 16:40:43 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(ScreenTimeApp());
 }
 
-class MyApp extends StatelessWidget {
+class ScreenTimeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Screen Time Tracker',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       home: HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Screen Time Tracker'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome 👋',
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
